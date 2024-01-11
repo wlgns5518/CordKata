166ce9b1327fbb8558737f49e9b09e317fa69e71
박지훈 <150890216+art-riel@users.noreply.github.com>
2024-01-11 09:15:12 +0900

    0111_ResolveDifference

---
+++ Difference/Difference/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("차이를 원하는 두수를 차례로 입력하세요.");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(solution(num1,num2));
+
+
+        }
+        static int solution(int num1, int num2)
+        {
+            int answer = num1-num2;
+            return answer;
+        }
+    }
+}
