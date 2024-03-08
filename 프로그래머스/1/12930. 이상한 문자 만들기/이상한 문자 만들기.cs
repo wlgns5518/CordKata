c222b32ebbc08da7fb19b4adbdf4e2a50264ea05
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2024-03-08 09:48:27 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.63 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public string solution(string s) {
+        string answer = "";
+        int num = 0;
+        
+        for(int i = 0; i < s.Length; i++)
+        {
+            answer += num % 2 == 0 ? char.ToUpper(s[i]) : char.ToLower(s[i]);
+            num = s[i] == ' ' ? 0 : num += 1;
+        }
+        return answer;
+    }
+}
