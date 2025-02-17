70f75e07211d7ad9b232c5569bdbfbe987c06a73
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-02-17 16:57:01 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 2.22 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.cs
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.cs
@@ -4,24 +4,8 @@ public class Solution {
     public string solution(string my_string)
 {
     string answer = "";
-    char[] chars= new char[my_string.Length];
-    my_string = my_string.ToLower();
-    for(int i=0;i<my_string.Length;i++)
-    {
-        chars[i] = my_string[i];
-    }
-    for (int i = 0; i < chars.Length; i++)
-    {
-        for(int j=0;j<chars.Length;j++)
-        {
-            if (chars[i] < chars[j])
-            {
-                char index = chars[i];
-                chars[i] = chars[j];
-                chars[j] = index;
-            }
-        }
-    }
+    char[] chars = my_string.ToLower().ToCharArray();
+    Array.Sort(chars);
     answer = new string(chars); 
     return answer;
 }
