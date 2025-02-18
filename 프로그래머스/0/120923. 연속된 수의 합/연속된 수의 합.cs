ea1bfe89c841f0bfb7c2bdea50b3704e4ea05bd0
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-02-18 16:55:08 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.48 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.cs
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.cs
@@ -1,18 +1,9 @@
 using System;
+using System.Linq;
 
 public class Solution {
-        public int[] solution(int num, int total)
-{
-    int middleNum = 0;
-    if ((total % num)!=0)
-        middleNum = total / num + 1;
-    else
-        middleNum = total / num;
-    int[] answer = new int[num];
-    for(int i=0;i< num;i++)
-    {
-        answer[i] = middleNum - num / 2 + i;
+    public int[] solution(int num, int total) {
+        int start = (2 * total / num - num + 1) / 2;
+        return Enumerable.Range(start, num).ToArray();
     }
-    return answer;
-}
 }
