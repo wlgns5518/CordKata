38288bebd8440fe1dbd4a88ff4ab6e13f9a0ce4b
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-05-20 20:51:29 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.76 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cs
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cs
@@ -1,32 +1,13 @@
+using System.Linq;
+
 public class Solution
 {
     public string solution(string s)
-    {
-        string[] sArray = s.Split(' ');
-        string[] UpperArray = new string[sArray.Length];
-        for (int i = 0; i < sArray.Length; i++)
-        {
-            if (!string.IsNullOrEmpty(sArray[i]))
-            {
-                UpperArray[i] = char.ToUpper(sArray[i][0]) + sArray[i].Substring(1).ToLower();
-            }
-            else
-            {
-                UpperArray[i] = sArray[i]; // 빈 문자열 처리
-            }
-        }
-        string answer = "";
-        for(int i=0;i<UpperArray.Length;i++)
-        {
-            if (i == UpperArray.Length - 1)
-            {
-                answer += UpperArray[i];
-            }
-            else
-            {
-                answer += UpperArray[i] + " ";
-            }
-        }
-        return answer;
-    }
+{
+    return string.Join(" ", s
+        .Split(' ')
+        .Select(word => string.IsNullOrEmpty(word) 
+            ? word 
+            : char.ToUpper(word[0]) + word.Substring(1).ToLower()));
+}
 }
