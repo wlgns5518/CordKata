b5d0dbf5d94ed9bb99d7fc39b6f1a1885d4ede7e
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-07-01 12:56:44 +0900

    [level 2] Title: 의상, Time: 3.53 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cs
+++ 프로그래머스/2/42578. 의상/의상.cs
@@ -1,8 +1,22 @@
 using System;
-using System.Linq;
+using System.Collections.Generic;
 
 public class Solution {
-    public int solution(string[,] clothes) {
-        return Enumerable.Range(0, clothes.GetLength(0)).Select(i => clothes[i,1]).GroupBy(p => p).Select(g => g.Count() + 1).Aggregate(1, (p, q) => p * q) - 1;
-    }
+     public int solution(string[,] clothes)
+ {
+     int answer = 1;
+     var clothesDict = new Dictionary<string, int>();
+     for (int i = 0; i < clothes.GetLength(0); i++)
+     {
+         if (clothesDict.ContainsKey(clothes[i, 1]))
+             clothesDict[clothes[i, 1]]++;
+         else
+             clothesDict[clothes[i, 1]] = 1;
+     }
+     foreach (var count in clothesDict.Values)
+     {
+         answer *= (count + 1);
+     }
+     return answer - 1;
+ }
 }
