565fea0db59f01099cae1a27cea8880670ab06ef
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-06 18:27:26 +0900

    [level 1] Title: 모의고사, Time: 1.51 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cs
+++ 프로그래머스/1/42840. 모의고사/모의고사.cs
@@ -1,39 +1,27 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Solution {
-    public int[] solution(int[] answers) {
-         int[] pattern1 = {1, 2, 3, 4, 5};
-        int[] pattern2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] pattern3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        
-        // 각 수포자의 정답 수를 카운트할 배열 초기화
-        int[] score = new int[3];
-        
-        // 정답 확인 및 점수 부여
-        for (int i = 0; i < answers.Length; i++) {
-            if (answers[i] == pattern1[i % pattern1.Length]) {
-                score[0]++;
-            }
-            if (answers[i] == pattern2[i % pattern2.Length]) {
-                score[1]++;
-            }
-            if (answers[i] == pattern3[i % pattern3.Length]) {
-                score[2]++;
-            }
-        }
-        
-        // 최고 점수 계산
-        int maxScore = Math.Max(score[0], Math.Max(score[1], score[2]));
-        
-        // 최고 점수를 받은 수포자들 찾기
-        List<int> winners = new List<int>();
-        for (int i = 0; i < score.Length; i++) {
-            if (score[i] == maxScore) {
-                winners.Add(i + 1);
-            }
-        }
-        
-        return winners.ToArray();
+    public int[] solution(int[] answers)
+{
+    int[] math1 = { 1, 2, 3, 4, 5};
+    int math1Len = math1.Length;
+    int[] math2 = { 2, 1, 2, 3, 2, 4, 2, 5 };
+    int math2Len = math2.Length;
+    int[] math3 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+    int math3Len = math3.Length;
+    int[] mathAns = new int[3];
+    for (int i = 0; i < answers.Length; i++)
+    {
+        if (answers[i] == math1[i % math1Len]) mathAns[0]++;
+        if (answers[i] == math2[i % math2Len]) mathAns[1]++;
+        if (answers[i] == math3[i % math3Len]) mathAns[2]++;
     }
+    List<int> lstAnswer = new List<int>();
+    if (mathAns[0] == mathAns.Max()) lstAnswer.Add(1);
+    if (mathAns[1] == mathAns.Max()) lstAnswer.Add(2);
+    if (mathAns[2] == mathAns.Max()) lstAnswer.Add(3);
+    return lstAnswer.ToArray();
+}
 }
