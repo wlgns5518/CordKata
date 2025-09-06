5f415199a750900d5a1e0794863497206ca36512
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-06 20:33:30 +0900

    [level 1] Title: 소수 만들기, Time: 0.69 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.cs
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cs
@@ -1,36 +1,36 @@
+using System;
+
 class Solution
 {
     public int solution(int[] nums)
+{
+    int answer = 0;
+    for(int i=0;i<nums.Length-2;i++)
+{
+    for(int j=i+1;j<nums.Length-1;j++)
     {
-        int answer = 0;
-        int sumNum;
-        bool isPrimeNum = false;
-        for(int i=0;i<nums.Length; i++)
+        for(int k=j+1;k<nums.Length;k++)
         {
-            for(int j=i+1;j<nums.Length; j++)
-            {
-                for(int k=j+1;k<nums.Length;k++)
-                {
-                    sumNum = nums[i] + nums[j] + nums[k];
-                    for(int o=2;o<sumNum;o++)
-                    {
-                        if (sumNum % o == 0)
-                        {
-                            isPrimeNum = false;
-                            break;
-                        }
-                        else
-                        {
-                            isPrimeNum = true;
-                        }
-                    }
-                    if(isPrimeNum == true)
-                    {
-                        answer++;
-                    }
-                }
-            }
+            if (IsPrime(nums[i] + nums[j] + nums[k]))
+                answer++;
+
         }
-        return answer;
     }
+}
+    return answer;
+}
+bool IsPrime(int number)
+{
+    if (number <= 1) return false; // 1 이하의 숫자는 소수가 아님
+    if (number <= 3) return true;  // 2와 3은 소수
+    if (number % 2 == 0 || number % 3 == 0) return false; // 2 또는 3으로 나누어지면 소수가 아님
+
+    for (int i = 5; i * i <= number; i += 6)
+    {
+        if (number % i == 0 || number % (i + 2) == 0)
+            return false;
+    }
+
+    return true;
+}
 }
