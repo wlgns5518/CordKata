7e6c3a6078408141f2c991579f622bb148a53ae5
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-06 13:18:49 +0900

    [level 1] Title: 과일 장수, Time: 247.12 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.cs
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cs
@@ -1,17 +1,21 @@
 using System;
 using System.Linq;
-public class Solution {
-    public int solution(int k, int m, int[] score) {
-        int answer = 0;
-        int index = m - 1;
-        Array.Sort(score);
-        Array.Reverse(score);
 
-        for (int i = 0; i < score.Length / m; i++)
-        {
-            answer += score[index] * m;
-            index += m;
-        }
-        return answer;
+public class Solution {
+    public int solution(int k, int m, int[] score)
+{
+    int answer = 0;
+    Array.Sort(score); // 오름차순 정렬
+    Array.Reverse(score); // 내림차순으로 변경
+
+    for (int i = 0; i + m <= score.Length; i += m)
+    {
+        // m개씩 그룹화하여 처리
+        int[] group = score.Skip(i).Take(m).ToArray();
+        int minInGroup = group[m - 1]; // 그룹에서 가장 작은 값
+        answer += minInGroup * m; // 그룹의 점수 계산
     }
+
+    return answer;
+}
 }
