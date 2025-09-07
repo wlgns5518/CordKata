1f9fc154168bf1ea51e5d57ebca708a3894037f8
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-08 02:31:58 +0900

    [level 1] Title: 기사단원의 무기, Time: 59.17 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cs
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cs
@@ -1,25 +1,28 @@
 using System;
 
-public class Solution
-{
+public class Solution {
     public int solution(int number, int limit, int power)
+{
+    int answer = 0;
+
+    for (int i = 1; i <= number; i++)
     {
-        int answer = 0;
-        for(int i=1;i<=number;i++)
+        int cnt = 0;
+
+        for (int j = 1; j*j <= i; j++)
         {
-            int count = 1;
-            for(int j=1;j<=i/2;j++)
+            if (i % j == 0)
             {
-                if(i%j==0)
-                    count++;
-                if (count > limit)
-                {
-                    count = power;
-                    break;
-                }
+                // j가 제곱근이면 한 번만 더하고, 아니면 짝으로 2개 추가
+                cnt += (j * j == i) ? 1 : 2;
+                // limit를 초과하면 더 계산할 필요 없음
+                if (cnt > limit) break;
             }
-            answer += count;
         }
-        return answer;
+
+        answer += (cnt > limit) ? power : cnt;
     }
+
+    return answer;
+}
 }
