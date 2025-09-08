34b275129885bc20fe333abe8e8590a74ab25571
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-08 11:40:33 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.22 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cs
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cs
@@ -1,91 +1,37 @@
 using System;
 
-public class Solution
-{
+public class Solution {
     public int[] solution(int[] lottos, int[] win_nums)
+{
+    int[] answer = new int[2];
+    int zeroCount = 0;
+    int correctCount = 0;
+    for(int i=0;i<6;i++)
     {
-        int[] answer = new int[2];
-        int zeroCount = 0;
-        int rightNum = 0;
-        for(int i=0;i<lottos.Length;i++)
+        for(int j=0;j<6;j++)
         {
             if (lottos[i] == 0)
+            {
                 zeroCount++;
-        }
-        for(int i=0;i<lottos.Length;i++)
-        {
-            for(int j=0;j<win_nums.Length;j++)
+                break;
+            }
+            if(lottos[i] == win_nums[j])
             {
-                if (lottos[i] == win_nums[j])
-                    rightNum++;
+                correctCount++;
+                break;
             }
         }
-        if(zeroCount !=0)
-        {
-            switch (rightNum)
-            {
-                case 0:
-                case 1:
-                    answer[0] = 6-zeroCount;
-                    if (answer[0] == 0)
-                        answer[0] = 1;
-                    answer[1] = 6;
-                    break;
-                case 2:
-                    answer[0] = 5 - zeroCount;
-                    answer[1] = 5;
-                    break;
-                case 3:
-                    answer[0] = 4 - zeroCount;
-                    answer[1] = 4;
-                    break;
-                case 4:
-                    answer[0] = 3 - zeroCount;
-                    answer[1] = 3;
-                    break;
-                case 5:
-                    answer[0] = 2 - zeroCount;
-                    answer[1] = 2;
-                    break;
-                case 6:
-                    answer[0] = 1;
-                    answer[1] = 1;
-                    break;
-
-            }
-        }
-        else
-        {
-            switch(rightNum)
-            {
-                case 0:
-                case 1:
-                    answer[0] = 6;
-                    answer[1] = 6;
-                    break;
-                case 2:
-                    answer[0] = 5;
-                    answer[1] = 5;
-                    break;
-                case 3:
-                    answer[0] = 4;
-                    answer[1] = 4;  
-                    break;
-                case 4:
-                    answer[0] = 3;
-                    answer[1] = 3;
-                    break;
-                case 5:
-                    answer[0] = 2;
-                    answer[1] = 2;
-                    break;
-                case 6:
-                    answer[0] = 1;
-                    answer[1] = 1;
-                    break;
-
-            }
-        }
-        return answer;
     }
+    answer[1] = 7 - correctCount;
+    if (answer[1] >6)
+    {
+        answer[1] = 6;
+    }
+    answer[0] = 7 - correctCount - zeroCount;
+    if (answer[0] > 6)
+    {
+        answer[0] = 6;
+    }
+    return answer;
+}
 }
