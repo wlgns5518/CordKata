b754bb94640ff96629c32462996e678c858296a0
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-14 21:24:27 +0900

    [level 0] Title: 문자열 출력하기, Time: 16.48 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.cs
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.cs
@@ -4,10 +4,7 @@ public class Example
 {
     public static void Main()
     {
-        String s;
-
-        Console.Clear();
-        s = Console.ReadLine();
+        String s = Console.ReadLine();
         Console.WriteLine(s);
 
     }
