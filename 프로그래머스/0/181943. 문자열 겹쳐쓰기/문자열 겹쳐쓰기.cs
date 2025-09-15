15851d6214f0789dea5d98305c0a6408b5b25cc1
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-15 19:07:46 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.20 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cs
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cs
@@ -1,7 +1,7 @@
 using System;
 
 public class Solution {
-    public string solution(string my_string, string overwrite_string, int s) {
+     public string solution(string my_string, string overwrite_string, int s) {
         string answer = "";
         answer = my_string.Remove(s,overwrite_string.Length);
         answer = answer.Insert(s, overwrite_string);
