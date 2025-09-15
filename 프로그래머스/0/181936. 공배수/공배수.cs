5475f01ef6abb0ae095c12f11bd3fa1000b921e6
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-15 21:26:11 +0900

    [level 0] Title: 공배수, Time: 0.24 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public int solution(int number, int n, int m) {
+        if(number % n ==0 && number % m ==0)
+            return 1;
+        return 0;
+    }
+}
