60953c80cd8a08c78a5c0a887cbb0a9eae8ca3ab
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-16 16:52:30 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 1.65 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.cs
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.cs
@@ -1,14 +1,12 @@
 using System;
+using System.Linq;
 
 public class Solution {
     public int solution(int[] num_list) {
-        int numSum = 0;
-        int numMul = 1;
-        for(int i=0;i<num_list.Length;i++)
-        {
-            numSum += num_list[i];
-            numMul *= num_list[i];
-        }
-        return numSum*numSum > numMul? 1:0;
+        int answer = 0;
+
+        answer = num_list.Aggregate(1, (x, y) => x * y) < Math.Pow(num_list.Sum(), 2) ? 1 : 0;
+
+        return answer;
     }
 }
