f509712ededf639cc3d0224873d920e41d90bee8
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-16 16:44:29 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.29 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int c) {
+        if(a==b && b==c)
+            return (a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c);
+        else if((a==b && b!=c)||(a!=b && b==c)||(a==c && b!=c))
+            return (a+b+c)*(a*a+b*b+c*c);
+        else
+            return a+b+c;
+    }
+}
