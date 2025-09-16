1183c82d29252924d8d9c0111e743b7ee830018a
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-16 19:56:27 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 1.49 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.cs
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.cs
@@ -2,25 +2,24 @@ using System;
 using System.Linq;
 
 public class Solution {
-    public int[] solution(int[] arr, int[,] queries) 
+    public int[] solution(int[] arr, int[,] queries)
+{
+    int[] answer = new int[queries.GetLength(0)];
+    for (int i = 0; i < queries.GetLength(0); i++)
     {
-        int[] answer = new int[queries.GetLength(0)];
-        for(int i = 0; i < queries.GetLength(0); ++i)
+        int minNum = int.MaxValue;
+        for (int j = queries[i, 0]; j <= queries[i, 1]; j++)
         {
-            int s = queries[i, 0];
-            int e = queries[i, 1];
-            int k = queries[i, 2];
-
-            int num = arr.Skip(s)
-                       .Take(e - s + 1)
-                       .Where(w => w > k)
-                       .DefaultIfEmpty(-1)
-                       .OrderBy(o => o)
-                       .First();
-
-            answer[i] = num;
+            if (arr[j] > queries[i, 2])
+            {
+                if (minNum >= arr[j])
+                    minNum = arr[j];
+            }
         }
-
-        return answer;
+        if (minNum == int.MaxValue)
+            minNum = -1;
+        answer[i] = minNum;
     }
+    return answer;
+}
 }
