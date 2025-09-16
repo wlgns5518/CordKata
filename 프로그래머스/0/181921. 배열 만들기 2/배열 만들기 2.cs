5bf8fab19aadc5281c20d639bcbd2f56255b22ce
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-16 20:33:41 +0900

    [level 0] Title: 배열 만들기 2, Time: 6.82 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.cs
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.cs
@@ -1,17 +1,29 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 public class Solution {
     public int[] solution(int l, int r) {
-        int[] answer = new int[] {};
-
-        answer = Enumerable.Range(l, r - l + 1).
-                   Where(x => x.ToString().All(y=> y == '5' || y == '0')).ToArray();
-
-        if(answer.Length <= 0)
-            return new int[] {-1};
-
-        return answer;
+        List<int> result = new List<int>();
+        
+        for(int i = l; i <= r; i++) {
+            int num = i;
+            bool valid = true;
+            
+            if(num == 0) valid = true; // 0도 포함 가능
+            
+            while(num > 0) {
+                int digit = num % 10;
+                if(digit != 0 && digit != 5) {
+                    valid = false;
+                    break;
+                }
+                num /= 10;
+            }
+            
+            if(valid) result.Add(i);
+        }
+        
+        if(result.Count == 0) return new int[]{-1};
+        return result.ToArray();
     }
 }
