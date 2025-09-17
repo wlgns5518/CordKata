fe56ecca7fbf501b85144d9f296294e2d6bc7e83
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-17 18:28:16 +0900

    [level 0] Title: qr code, Time: 1.34 ms, Memory: 32 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.cs
+++ 프로그래머스/0/181903. qr code/qr code.cs
@@ -3,11 +3,7 @@ using System;
 public class Solution {
     public string solution(int q, int r, string code) {
         string answer = "";
-        for(int i=0;i<code.Length;i++)
-        {
-            if(i%q == r)
-                answer += code[i];
-        }
+        for(int i=r;i<code.Length;i+=q) answer+=code[i];
         return answer;
     }
 }
