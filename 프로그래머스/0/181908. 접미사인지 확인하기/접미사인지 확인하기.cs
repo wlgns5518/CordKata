2efed2e77d0f16517c2d160a28cf3adcee05baf6
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-17 17:51:37 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 4.82 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cs
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cs
@@ -2,6 +2,8 @@ using System;
 
 public class Solution {
     public int solution(string my_string, string is_suffix) {
-        return my_string.EndsWith(is_suffix) ? 1 : 0;
+        if(my_string.Length < is_suffix.Length) return 0;
+        if(my_string.IndexOf(is_suffix, my_string.Length - is_suffix.Length ) >= 0) return 1;
+        return 0;
     }
 }
