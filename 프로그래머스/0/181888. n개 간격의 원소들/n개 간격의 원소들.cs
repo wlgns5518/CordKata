c5776d37ec234e3fdfcfeb9ae5a3cf006870e10a
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-18 17:54:58 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.27 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.cs
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.cs
@@ -2,11 +2,8 @@ using System;
 
 public class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[(num_list.Length-1)/n+1];
-        for(int i=0;i<answer.Length;i++)
-        {
-            answer[i] = num_list[i*n];
-        }
+        int[] answer = new int[(num_list.Length+n-1)/n];
+        for(int i =0;i<answer.Length;i++) answer[i]=num_list[i*n];
         return answer;
     }
 }
