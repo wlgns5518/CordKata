752c6372e15b10b97f006e3a05519c5b653f0522
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-19 16:00:39 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 0.86 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.cs
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.cs
@@ -1,14 +1,20 @@
 using System;
-using System.Collections.Generic;
+using System.Linq;
 
 public class Solution {
     public int[] solution(int[] arr) {
-        var answer = new List<int>();
-        for(int i=0;i<arr.Length;i++)
+        // 결과 배열 길이는 arr 원소 합
+        int[] answer = new int[arr.Sum()];
+        int idx = 0; // 결과 배열 현재 위치
+
+        for(int i = 0; i < arr.Length; i++)
         {
-            for(int j=0;j<arr[i];j++)
-                answer.Add(arr[i]);
+            for(int j = 0; j < arr[i]; j++)
+            {
+                answer[idx++] = arr[i]; // idx를 하나씩 증가
+            }
         }
-        return answer.ToArray();
+
+        return answer;
     }
 }
