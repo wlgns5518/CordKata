c9a8657f060168e6dabc2ede569ff46cad18e07b
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-19 14:23:47 +0900

    [level 0] Title: 1로 만들기, Time: 0.35 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.cs
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.cs
@@ -1,28 +1,20 @@
 using System;
 
 public class Solution {
-    public int solution(int[] num_list) {
-        int answer = 0; 
-        bool isAllOne = true;
-        while(isAllOne)
+    public int solution(int[] num_list)
+{
+    int answer = 0;
+    for(int i=0;i<num_list.Length;i++)
+    {
+        while (num_list[i] !=1)
         {
-            int count = 0;
-            for(int i=0;i<num_list.Length;i++)
-            {
-                if(num_list[i]==1)
-                    count++;
-                else
-                {
-                    answer++;
-                    if(num_list[i] %2 == 0)
-                        num_list[i]/=2;
-                    else
-                        num_list[i] = (num_list[i]-1)/2;
-                }
-            }
-            if(count == num_list.Length)
-                isAllOne = false;
+            answer++;
+            if (num_list[i] % 2 == 0)
+                num_list[i] /= 2;
+            else
+                num_list[i] = (num_list[i] - 1) / 2;
         }
-        return answer;
     }
+    return answer;
+}
 }
