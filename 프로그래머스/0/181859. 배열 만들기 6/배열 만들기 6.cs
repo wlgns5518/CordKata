c85be7cfd56d07cd958759e250753bf706c7ed6f
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-19 16:27:56 +0900

    [level 0] Title: 배열 만들기 6, Time: 20.85 ms, Memory: 48.6 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.cs
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.cs
@@ -1,24 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Solution {
-    public int[] solution(int[] arr) {
-        var answer = new Stack<int>();
-        for(int i=0; i < arr.Length; i++)
+    public int[] solution(int[] arr) 
+    {
+        var stk = new List<int>();
+        for(int i = 0; i < arr.Length; ++i)
         {
-            if(answer.Count ==0)
-                answer.Push(arr[i]);
-            else if(answer.Peek() == arr[i])
-                answer.Pop();
-            else if(answer.Peek() != arr[i])
-                answer.Push(arr[i]);
+            if(stk.Count == 0)
+                stk.Add(arr[i]);
+            else
+            {
+                if(stk[stk.Count - 1] == arr[i])
+                    stk.RemoveAt(stk.Count - 1);
+                else
+                    stk.Add(arr[i]);
+            }
         }
-        if(answer.Count == 0) answer.Push(-1);
-        int[] answerArr = new int[answer.Count];
-        for(int i = answer.Count-1; i>=0;i--)
-        {
-            answerArr[i] = answer.Pop();
-        }
-        return answerArr;
+        return stk.Count > 0 ? stk.ToArray() : new int[1]{ -1 };
     }
 }
