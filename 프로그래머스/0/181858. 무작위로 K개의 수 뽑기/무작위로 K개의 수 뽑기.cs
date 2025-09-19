654fc13eff514edad7b584c7313c3a907581e8ee
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-19 17:28:41 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 15.90 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.cs
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.cs
@@ -1,13 +1,25 @@
 using System;
-using System.Linq;
+using System.Collections.Generic;
 
-public class Solution {
-    public int[] solution(int[] arr, int k) 
+public class Solution
+{
+    public int[] solution(int[] arr, int k)
     {
-        int[] distinct = arr.Distinct().Take(k).ToArray();
-        int[] answer = Enumerable.Repeat(-1, k).ToArray();
-        Array.Copy(distinct, 0, answer, 0, distinct.Length);
+        List<int> answerList = new List<int>();
 
-        return answer;
+        foreach (int num in arr)
+        {
+            if (!answerList.Contains(num))
+            {
+                answerList.Add(num);
+                if (answerList.Count == k) break;
+            }
+        }
+
+        // 부족하면 -1로 채우기
+        while (answerList.Count < k)
+            answerList.Add(-1);
+
+        return answerList.ToArray();
     }
 }
