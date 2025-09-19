dc56c0a4838b22db5079b5de3ef7877e5c50d3d8
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-19 15:27:42 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 6.92 ms, Memory: 32.7 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.cs
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.cs
@@ -1,10 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 public class Solution {
     public string[] solution(string myString)
-{
-    string[] answer = myString.Split("x", System.StringSplitOptions.RemoveEmptyEntries);
-    Array.Sort(answer);
-    return answer;
-}
+    {
+        List<string> parts = new List<string>();
+        int start = 0;
+        
+        for (int i = 0; i < myString.Length; i++)
+        {
+            if (myString[i] == 'x')
+            {
+                if (i > start) // 빈 문자열 제거
+                    parts.Add(myString.Substring(start, i - start));
+                start = i + 1;
+            }
+        }
+        if (start < myString.Length)
+            parts.Add(myString.Substring(start));
+
+        parts.Sort(StringComparer.Ordinal); // 빠른 정렬
+        return parts.ToArray();
+    }
 }
