6ece540520fecebff888aa0c80c93dcb4a0d3415
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-19 19:03:13 +0900

    [level 0] Title: 문자열 묶기, Time: 1.34 ms, Memory: 44.5 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.cs
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.cs
@@ -4,7 +4,6 @@ using System.Linq;
 public class Solution {
     public int solution(string[] strArr) {
         int[] count = new int[31];
-        int answer = 0;
 
         for(int i = 0;i < strArr.Length;i++)
         {
