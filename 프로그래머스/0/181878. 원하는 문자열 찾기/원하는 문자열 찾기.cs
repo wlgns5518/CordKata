a67c441adb65a664f7cd48080e081584ee52b4b5
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-19 14:35:00 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 5.18 ms, Memory: 31.8 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cs
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cs
@@ -3,10 +3,7 @@ using System.Linq;
 
 public class Solution {
     public int solution(string myString, string pat) {
-        // 모두 소문자로 변환
-        string lowerMyString = myString.ToLower();
-        string lowerPat = pat.ToLower();
 
-        return lowerMyString.Contains(lowerPat) ? 1 : 0;
+        return myString.ToUpper().IndexOf(pat.ToUpper()) == -1 ? 0 : 1;
     }
 }
