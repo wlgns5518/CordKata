94bd47f6723982399717d1a763be63b5b3ed2b78
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-20 13:18:17 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 1.82 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.cs
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.cs
@@ -5,14 +5,33 @@ using System.Linq;
 public class Solution {
     public int solution(int[] rank, bool[] attendance)
 {
-    var list = new List<(int rank, int index)>();
-    for (int i = 0; i < rank.Length; ++i)
+    int n = rank.Length;
+    
+    // 출석한 학생들의 (등수, 번호) 저장
+    List<(int rank, int index)> attendedStudents = new List<(int, int)>();
+    
+    for (int i = 0; i < n; i++)
     {
-        if (!attendance[i]) continue;
-        list.Add((rank[i], i));
+        if (attendance[i])
+        {
+            attendedStudents.Add((rank[i], i));
+        }
     }
-
-    var arr = list.OrderBy(o => o.Item1).Take(3).ToArray();
-    return 10000 * arr[0].Item2 + 100 * arr[1].Item2 + arr[2].Item2;
+    
+    // 등수를 기준으로 정렬 (오름차순)
+    attendedStudents.Sort((x, y) => x.rank.CompareTo(y.rank));
+    
+    // 상위 3명이 있는지 확인
+    if (attendedStudents.Count < 3)
+    {
+        throw new InvalidOperationException("출석한 학생이 3명 미만입니다.");
+    }
+    
+    // 상위 3명 학생 번호 추출
+    int a = attendedStudents[0].index;  // 1등
+    int b = attendedStudents[1].index;  // 2등
+    int c = attendedStudents[2].index;  // 3등
+    
+    return 10000 * a + 100 * b + c;
 }
 }
