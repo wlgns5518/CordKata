c1c30ff7f77aa75dc03fd1fa30d8120d1bd509f3
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-20 12:56:39 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 3.45 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cs
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cs
@@ -3,6 +3,8 @@ using System.Linq;
 
 public class Solution {
     public int[] solution(int[] num_list) {
-        return num_list.OrderBy(o => o).Skip(5).ToArray();
+        Array.Sort(num_list);
+        int[] answer = num_list.Skip(5).ToArray();
+        return answer;
     }
 }
