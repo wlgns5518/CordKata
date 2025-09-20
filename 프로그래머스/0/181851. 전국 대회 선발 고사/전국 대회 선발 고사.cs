4712077e2b6aa55d1b6a1bf2823a337493984245
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-09-20 13:07:49 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 4.94 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.cs
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.cs
@@ -1,40 +1,18 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Solution {
-    public int solution(int[] rank, bool[] attendance) {
-        int n = rank.Length;
-        // (등수, 번호) 쌍 저장
-        int[,] students = new int[n, 2];
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (attendance[i]) {
-                students[count, 0] = rank[i]; // 등수
-                students[count, 1] = i;       // 학생 번호
-                count++;
-            }
-        }
-
-        // 단순 정렬 (버블 정렬 또는 선택 정렬)
-        for (int i = 0; i < count - 1; i++) {
-            for (int j = i + 1; j < count; j++) {
-                if (students[i, 0] > students[j, 0]) {
-                    // swap (등수, 번호) 같이 바꿔주기
-                    int tmpRank = students[i, 0];
-                    int tmpIdx  = students[i, 1];
-                    students[i, 0] = students[j, 0];
-                    students[i, 1] = students[j, 1];
-                    students[j, 0] = tmpRank;
-                    students[j, 1] = tmpIdx;
-                }
-            }
-        }
-
-        // 상위 3명 학생 번호
-        int a = students[0, 1];
-        int b = students[1, 1];
-        int c = students[2, 1];
-
-        return 10000 * a + 100 * b + c;
+    public int solution(int[] rank, bool[] attendance)
+{
+    var list = new List<(int rank, int index)>();
+    for (int i = 0; i < rank.Length; ++i)
+    {
+        if (!attendance[i]) continue;
+        list.Add((rank[i], i));
     }
+
+    var arr = list.OrderBy(o => o.Item1).Take(3).ToArray();
+    return 10000 * arr[0].Item2 + 100 * arr[1].Item2 + arr[2].Item2;
+}
 }
