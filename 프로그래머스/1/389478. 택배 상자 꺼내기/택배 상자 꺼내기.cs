88de3da3af07dfba1822d9bee9b0ce74c286fa5c
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-03 22:22:33 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.30 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cs
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cs
@@ -2,23 +2,21 @@ using System;
 
 public class Solution {
     public int solution(int n, int w, int num) {
-    int[] height = new int[w];
 
-    // 각 열별로 박스 개수 채우기
-    for (int i = 0; i < n; i++) {
-        int row = i / w;
-        int col = (row % 2 == 0) ? i % w : (w - 1 - (i % w));
-        height[col]++;
+        int count = 0;
+        while(n >= num)
+        {
+            //꺼내기 카운트
+            count++;             
+            //다음위치 계산        
+            if(num%w==0)
+            {
+                num++;
+            }else
+            {
+                num += (((((num/w)+1)*w)-num) * 2 ) + 1;
+            }
+        }       
+        return count;
     }
-
-    // num 위치 찾기
-    int idx = num - 1;
-    int rowNum = idx / w;
-    int colNum = (rowNum % 2 == 0) ? (idx % w) : (w - 1 - (idx % w));
-
-    // 해당 열에 총 몇 개 쌓였는지 - (num이 몇 번째 행인지) = 위에 몇 개 남아있는지
-    int answer = height[colNum] - rowNum;
-
-    return answer;
-}
 }
