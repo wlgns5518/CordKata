1201c8662412d7e18a3f6fca332e2d1cbeda2371
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-07 20:32:55 +0900

    [level 0] Title: 문자열 정수의 합, Time: 1.52 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.cs
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.cs
@@ -1,15 +1,8 @@
 using System;
+using System.Linq;
 
 public class Solution {
-    public int solution(string num_str)
-    {
-        int answer = 0;
-        int num = 0;
-        for (int i = 0; i < num_str.Length; i++)
-        {
-            int.TryParse(num_str[i].ToString(), out num);
-            answer += num;
-        }
-        return answer;
+    public int solution(string num_str) {
+        return num_str.Select(s => s - '0').Sum();
     }
 }
