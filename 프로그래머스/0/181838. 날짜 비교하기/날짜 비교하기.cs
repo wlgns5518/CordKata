4feb75771f693da3004ab5f5c91aec77f0693141
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-07 21:49:48 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.29 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cs
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cs
@@ -2,9 +2,16 @@ using System;
 
 public class Solution {
     public int solution(int[] date1, int[] date2) {
-        DateTime d1 = new DateTime(date1[0], date1[1], date1[2]);
-        DateTime d2 = new DateTime(date2[0], date2[1], date2[2]);
-
-        return d1 < d2 ? 1 : 0;
+        int answer = 0;
+        for(int i=0;i<3;i++)
+        {
+            if(date1[i]<date2[i])
+                answer = 1;
+            else if(date1[i] == date2[i])
+                continue;
+            else
+                break;
+        }
+        return answer;
     }
 }
