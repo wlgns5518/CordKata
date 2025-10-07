5e975917e03f70bacadaf1cd2c2e9709b13069fe
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-07 21:34:31 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.40 ms, Memory: 30.9 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.cs
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.cs
@@ -7,9 +7,8 @@ public class Solution {
     var answer = new StringBuilder();
     for (int i = 0; i < str_list.Length; i++)
     {
-        if (str_list[i].Contains(ex))
-            continue;
-        answer.Append(str_list[i]);
+        if (!str_list[i].Contains(ex))
+            answer.Append(str_list[i]);
     }
     return answer.ToString();
 }
