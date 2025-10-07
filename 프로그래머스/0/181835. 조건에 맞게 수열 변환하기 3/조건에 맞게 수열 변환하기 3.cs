7a5e718ed9d1f59aee5b3473863fabe817c97ee0
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-07 22:29:17 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 3.15 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] arr, int k) {
+        int[] answer = arr;
+        if(k%2 != 0)
+        {
+            for(int i=0;i<answer.Length;i++)
+                answer[i] *= k;
+        }
+        else
+        {
+            for(int i=0;i<answer.Length;i++)
+                answer[i] += k;
+        }
+        return answer;
+    }
+}
