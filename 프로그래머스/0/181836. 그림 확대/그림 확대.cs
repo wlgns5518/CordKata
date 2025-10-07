da7460971584e4eafd3f0b88c0925d0aa52fb3af
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-07 22:16:52 +0900

    [level 0] Title: 그림 확대, Time: 0.52 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.cs
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.cs
@@ -1,24 +1,25 @@
 using System;
+using System.Text;
 
 public class Solution {
     public string[] solution(string[] picture, int k)
 {
     string[] answer = new string[picture.Length * k];
     int index = 0;
-
+    var expandedRow = new StringBuilder();
     foreach (string s in picture)
     {
         // 가로로 k배 확장
-        string expandedRow = "";
+        expandedRow.Clear();
         foreach (char ch in s)
         {
-            expandedRow += new string(ch, k);
+            expandedRow.Append(ch, k);
         }
 
         // 세로로 k배 확장
         for (int i = 0; i < k; i++)
         {
-            answer[index++] = expandedRow;
+            answer[index++] = expandedRow.ToString();
         }
     }
 
