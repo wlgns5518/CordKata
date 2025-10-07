024e81b8b2e61c25a5c14b118c5fa55be528256c
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-07 21:37:36 +0900

    [level 0] Title: 정수 찾기, Time: 0.86 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.cs
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.cs
@@ -1,13 +1,8 @@
 using System;
+using System.Linq;
 
 public class Solution {
     public int solution(int[] num_list, int n) {
-        int answer = 0;
-        for(int i=0;i<num_list.Length;i++)
-        {
-            if(num_list[i] == n)
-                answer = 1;
-        }
-        return answer;
+        return num_list.Contains(n) ? 1 : 0;
     }
 }
