53d6a851c66e5980e38e139839084440f03b255a
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-07 21:01:13 +0900

    [level 0] Title: 두 수의 합, Time: 6310.90 ms, Memory: 49.9 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.cs
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.cs
@@ -1,24 +1,28 @@
 using System;
-using System.Text;
 
 public class Solution {
     public string solution(string a, string b) {
+        string result = "";
+        int carry = 0;
         int i = a.Length - 1;
         int j = b.Length - 1;
-        int carry = 0;
-        StringBuilder result = new StringBuilder();
-
         while (i >= 0 || j >= 0 || carry > 0)
         {
-            int x = i >= 0 ? a[i] - '0' : 0;
-            int y = j >= 0 ? b[j] - '0' : 0;
-            int sum = x + y + carry;
+            int sum = carry;
+            if (i >= 0)
+            {
+                sum += a[i] - '0';
+                i--;
+            }
+            if (j >= 0)
+            {
+                sum += b[j] - '0';
+                j--;
+            }
             carry = sum / 10;
-            result.Insert(0, sum % 10);
-            i--;
-            j--;
+            int digit = sum % 10;
+            result = digit.ToString() + result;
         }
-
-        return result.ToString();
-    }
+        return result;
+        }
 }
