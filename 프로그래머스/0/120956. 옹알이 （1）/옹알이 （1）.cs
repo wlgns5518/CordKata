de2e5b2498adce003d5aa72c06f8dd326f771a54
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-09 21:44:12 +0900

    [level 0] Title: 옹알이 (1), Time: 1.48 ms, Memory: 31.8 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.cs
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.cs
@@ -3,20 +3,16 @@ using System;
 public class Solution {
     public int solution(string[] babbling) {
         int answer = 0;
-        string[] talkable = { "aya", "ye", "woo", "ma" };
-        for (int i = 0; i < babbling.Length; i++) 
+        int num = 0;
+        for(int i=0;i<babbling.Length;i++)
         {
-            string word = babbling[i];
-            foreach (string able in talkable)
-            {
-                word = word.Replace(able, " ");
-            }
-            if(string.IsNullOrWhiteSpace(word))
-            {
+            babbling[i] = babbling[i].Replace("aya","1");
+            babbling[i] = babbling[i].Replace("ye","1");
+            babbling[i] = babbling[i].Replace("woo","1");
+            babbling[i] = babbling[i].Replace("ma","1");
+            if(int.TryParse(babbling[i], out num))
                 answer++;
-            }
         }
-
         return answer;
     }
 }
