5148c4a8e9d1eaf99b4922b4229fe6499996f7c5
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-09 22:56:42 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.43 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cs
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cs
@@ -1,10 +1,33 @@
 using System;
 
 public class Solution {
-    public int solution(string str1, string str2) {
-        int answer = 2;
-        if(str1.Contains(str2))
-            answer = 1;
-        return answer;
+    public int solution(string str1, string str2) 
+    {
+        if (str1.Length < str2.Length)
+            return 2;
+        else if (str1.Length == str2.Length)
+        {
+            if (str1 == str2)
+                return 1;
+            else
+                return 2;
+        }
+
+        for (int i = 0; i <= str1.Length - str2.Length; i++)
+        {
+            int count = 0;   
+            for (int j = 0; j < str2.Length; j++)
+            {
+                if (str1[i + j] == str2[j])
+                {
+                    count++;
+                    if (count == str2.Length)
+                        return 1;
+                }
+                else
+                    break;   
+            }
+        }
+        return 2;
     }
 }
