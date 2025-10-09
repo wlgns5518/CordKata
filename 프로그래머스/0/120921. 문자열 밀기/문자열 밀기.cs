b7e9d8d700d2337c4dd161fb22ab5335204d9ac9
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-09 22:06:28 +0900

    [level 0] Title: 문자열 밀기, Time: 4.93 ms, Memory: 31.8 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.cs
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.cs
@@ -2,17 +2,7 @@ using System;
 
 public class Solution {
     public int solution(string A, string B) {
-        if (A.Length != B.Length) return -1;
-
-        string rotated = A;
-        int n = A.Length;
-
-        for (int i = 0; i < n; i++) {
-            if (rotated == B) return i; // 현재 회전 횟수가 최소
-            // 오른쪽으로 한 칸 회전
-            rotated = rotated[ n - 1 ] + rotated.Substring(0, n - 1);
-        }
-
-        return -1; // 어떤 회전으로도 B가 안 되는 경우
+        int answer = (B+B).IndexOf(A);
+        return answer;
     }
 }
