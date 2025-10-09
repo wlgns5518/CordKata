2c0c6f15133442d655f7acc67dc21a64e79ef041
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-09 22:38:47 +0900

    [level 0] Title: 7의 개수, Time: 0.22 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.cs
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.cs
@@ -1,18 +1,18 @@
 using System;
 
 public class Solution {
-    public int solution(int[] array)
-{
-    int answer = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        string arraystr = array[i].ToString();
-        for(int j = 0; j <arraystr.Length;j++ )
+    public int solution(int[] array) {
+        int answer = 0;
+        foreach(int i in array)
         {
-            if(arraystr[j] == '7')
-                answer++;
+            int temp = i;
+            while(temp>0)
+            {
+                if(temp%10==7)
+                    answer++;
+                temp/=10;
+            }
         }
+        return answer;
     }
-    return answer;
-}
 }
