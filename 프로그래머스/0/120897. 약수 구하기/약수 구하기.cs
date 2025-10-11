e036332122e45eb59f85c2da1a4c131eb1ea7f5e
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-11 20:15:15 +0900

    [level 0] Title: 약수 구하기, Time: 2.07 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.cs
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.cs
@@ -1,14 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
-public class Solution {
-    public int[] solution(int n) {
-        var answer = new List<int>();
-        for(int i=1;i<=n;i++)
+public class Solution
+{
+    public int[] solution(int n)
+    {
+        List<int> divisors = new List<int>();
+        
+        for (int i = 1; i * i <= n; i++)
         {
-            if(n%i == 0)
-                answer.Add(i);
+            if (n % i == 0)
+            {
+                divisors.Add(i);
+                if (i != n / i)
+                    divisors.Add(n / i);
+            }
         }
-        return answer.ToArray();
+        
+        divisors.Sort();
+        return divisors.ToArray();
     }
 }
