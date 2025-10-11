edf0fc98967dd4d1174823b41150efd713c199c0
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-11 21:46:45 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.18 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int chicken) {
+        int answer = 0;
+        while(chicken>=10)
+        {
+            answer += chicken/10;
+            chicken = chicken/10 + chicken % 10;
+        }
+        return answer;
+    }
+}
