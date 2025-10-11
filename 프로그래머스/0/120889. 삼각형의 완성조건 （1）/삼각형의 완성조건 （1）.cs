000d8b86460b51b3e3f96dd3b9822240824f65f0
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-11 21:06:40 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.19 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.cs
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.cs
@@ -1,7 +1,19 @@
 using System;
-using System.Linq;
-public class Solution {
-    public int solution(int[] sides) {
-        return sides.Max() < (sides.Sum() - sides.Max()) ? 1 : 2;
+
+public class Solution
+{
+    public int solution(int[] sides)
+    {
+        int a = sides[0];
+        int b = sides[1];
+        int c = sides[2];
+
+        // 최대값 찾기
+        int max = Math.Max(a, Math.Max(b, c));
+
+        // 나머지 두 변의 합
+        int sumOther = a + b + c - max;
+
+        return max < sumOther ? 1 : 2;
     }
 }
