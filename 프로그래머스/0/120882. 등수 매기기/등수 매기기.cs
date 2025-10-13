68e25ded8627dd3b2ac5d7e25eee7c7f43234177
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-13 20:41:38 +0900

    [level 0] Title: 등수 매기기, Time: 0.39 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int[,] score) {
+        int n = score.GetLength(0);
+        double[] avg = new double[n];
+        int[] rank = new int[n];
+        
+        // 평균 계산
+        for (int i = 0; i < n; i++) {
+            avg[i] = (score[i, 0] + score[i, 1]) / 2.0;
+        }
+
+        // 등수 계산
+        for (int i = 0; i < n; i++) {
+            rank[i] = 1; // 기본 등수는 1등
+            for (int j = 0; j < n; j++) {
+                if (avg[j] > avg[i]) {
+                    rank[i]++;
+                }
+            }
+        }
+
+        return rank;
+    }
+}
