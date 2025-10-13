032e3b0609c5e623607e7cb291301e63f5677405
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-13 20:57:15 +0900

    [level 0] Title: 특이한 정렬, Time: 0.41 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.cs
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.cs
@@ -1,11 +1,28 @@
 using System;
-using System.Linq;
 
 public class Solution {
     public int[] solution(int[] numlist, int n) {
-        return numlist
-            .OrderBy(x => Math.Abs(x - n))   // n과의 거리 기준 오름차순 정렬
-            .ThenByDescending(x => x)        // 거리가 같다면 큰 수가 앞에 오도록 정렬
-            .ToArray();
+        int len = numlist.Length;
+
+        // 버블 정렬
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - i - 1; j++) {
+
+                int distA = Math.Abs(numlist[j] - n);
+                int distB = Math.Abs(numlist[j + 1] - n);
+
+                // 정렬 조건:
+                // 1️⃣ n과의 거리가 더 크면 뒤로
+                // 2️⃣ 거리가 같고 더 작은 수면 뒤로
+                if (distA > distB || (distA == distB && numlist[j] < numlist[j + 1])) {
+                    // swap
+                    int temp = numlist[j];
+                    numlist[j] = numlist[j + 1];
+                    numlist[j + 1] = temp;
+                }
+            }
+        }
+
+        return numlist;
     }
 }
