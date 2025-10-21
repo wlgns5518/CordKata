c83309cad96f1b84a0609f7fc2659724f27072d0
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-21 12:29:07 +0900

    [level 0] Title: 컨트롤 제트, Time: 1.81 ms, Memory: 31.8 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.cs
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.cs
@@ -1,21 +1,40 @@
 using System;
+using System.Collections.Generic;
 
 public class Solution {
-   public int solution(string s)
+public int solution(string s)
 {
     int answer = 0;
     int num = 0;
-    string[] sArray = s.Split();
-    for (int i = 0; i < sArray.Length; i++)
+    int start = 0;
+
+    Stack<int> stack = new Stack<int>();
+
+    for (int i = 0; i <= s.Length; i++)
     {
-        if (sArray[i] == "Z")
-            answer -= num;
-        else
+        if (i == s.Length || s[i] == ' ')
         {
-            int.TryParse(sArray[i], out num );
-            answer += num;
+            if (start < i)
+            {
+                string token = s.Substring(start, i - start);
+                if (token == "Z")
+                {
+                    if (stack.Count > 0)
+                    {
+                        answer -= stack.Pop();
+                    }
+                }
+                else
+                {
+                    num = int.Parse(token);
+                    stack.Push(num);
+                    answer += num;
+                }
+            }
+            start = i + 1;
         }
     }
+
     return answer;
 }
 }
