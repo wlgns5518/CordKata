083d7efa7c99eeeee65f2d2af90a534c511fd72c
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-21 15:45:03 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 1.81 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cs
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cs
@@ -4,22 +4,8 @@ public class Solution {
     public int solution(int[] numbers)
 {
     int answer = 0;
-    int maxNum = 0;
-    int secNum = 0;
-    for(int i=0;i<numbers.Length;i++)
-    {
-        if (secNum < numbers[i])
-        {
-            if (maxNum < numbers[i])
-            {
-                secNum = maxNum;
-                maxNum = numbers[i];
-            }
-            else
-                secNum = numbers[i];
-        }
-    }
-    answer = maxNum * secNum;
+    Array.Sort(numbers);
+    answer = numbers[numbers.Length-1] * numbers[numbers.Length-2];
     return answer;
 }
 }
