8d9558f7a8dc4adca728dc281a8ac53530715af8
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-28 20:35:45 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.30 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.cs
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.cs
@@ -2,30 +2,14 @@ using System;
 
 public class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        int[] answer = new int[2];
-        int lcm = LCM(denom1,denom2);
-        int factor1 = lcm/denom1;
-        int factor2 = lcm/denom2;
-        answer[0] = factor1 * numer1 + factor2 * numer2;
-        answer[1] = lcm;
-        int gcd = GCD(answer[0], answer[1]);
-answer[0] /= gcd;
-answer[1] /= gcd;
-        return answer;
-    }
-    int GCD(int a, int b)
-    {
-        while (b != 0)
-        {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
+        int lcm = denom1 / GCD(denom1, denom2) * denom2;  // 공통 분모
+        int sumNumerator = numer1 * (lcm / denom1) + numer2 * (lcm / denom2);
+        int gcd = GCD(sumNumerator, lcm);                  // 약분
+        return new int[] { sumNumerator / gcd, lcm / gcd };
     }
 
-    int LCM(int a, int b)
-    {
-        return a / GCD(a, b) * b;
+    int GCD(int a, int b) {
+        while (b != 0) { int t = b; b = a % b; a = t; }
+        return a;
     }
 }
