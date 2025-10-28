5eb0699962196c2a4769957ebe6f80c09041de65
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-28 19:40:18 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 30.10 ms, Memory: 16.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String[] s;
+
+        Console.Clear();
+        s = Console.ReadLine().Split(' ');
+
+        int n = Int32.Parse(s[0]);
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=0;j<i;j++)
+                Console.Write("*");
+            Console.WriteLine();
+        }
+    }
+}
