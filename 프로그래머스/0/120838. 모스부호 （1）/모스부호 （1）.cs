32568e580a0717e7a60db7a7b6622cf429476dbd
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-28 18:41:34 +0900

    [level 0] Title: 모스부호 (1), Time: 1.96 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.cs
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.cs
@@ -1,28 +1,28 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 public class Solution
 {
     public string solution(string letter)
     {
-        Dictionary<string, char> morse = new Dictionary<string, char>()
+        // static readonly 로 미리 정의하면 매번 새로 안 만들어도 됨
+        var morse = new Dictionary<string, char>()
         {
-            {".-", 'a'},   {"-...", 'b'}, {"-.-.", 'c'}, {"-..", 'd'}, {".", 'e'},
-            {"..-.", 'f'}, {"--.", 'g'},  {"....", 'h'}, {"..", 'i'},  {".---", 'j'},
-            {"-.-", 'k'},  {".-..", 'l'}, {"--", 'm'},   {"-.", 'n'},  {"---", 'o'},
-            {".--.", 'p'}, {"--.-", 'q'}, {".-.", 'r'},  {"...", 's'}, {"-", 't'},
-            {"..-", 'u'},  {"...-", 'v'}, {".--", 'w'},  {"-..-", 'x'},{"-.--", 'y'},
-            {"--..", 'z'}
+            [".-"]='a', ["-..."]='b', ["-.-."]='c', ["-.."]='d', ["."]='e',
+            ["..-."]='f', ["--."]='g', ["...."]='h', [".."]='i', [".---"]='j',
+            ["-.-"]='k', [".-.."]='l', ["--"]='m', ["-."]='n', ["---"]='o',
+            [".--."]='p', ["--.-"]='q', [".-."]='r', ["..."]='s', ["-"]='t',
+            ["..-"]='u', ["...-"]='v', [".--"]='w', ["-..-"]='x', ["-.--"]='y', ["--.."]='z'
         };
 
-        string[] words = letter.Split(' ');
-        string result = "";
+        // StringBuilder 사용 → 문자열 연결 비용 감소
+        var sb = new StringBuilder();
 
-        foreach (string code in words)
-        {
-            result += morse[code];
-        }
+        // Split() 결과를 한 번에 순회
+        foreach (var code in letter.Split(' '))
+            sb.Append(morse[code]);
 
-        return result;
+        return sb.ToString();
     }
 }
