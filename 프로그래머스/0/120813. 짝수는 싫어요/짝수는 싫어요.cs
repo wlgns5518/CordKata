6bc6960057574afeb034a00341e059bda747d3fd
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-28 20:22:48 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.24 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cs
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cs
@@ -1,13 +1,10 @@
 using System;
-using System.Collections.Generic;
+
 public class Solution {
     public int[] solution(int n) {
-        var answer = new List<int>(n/2);
-        for(int i=1;i<=n;i++)
-        {
-            if(i%2 != 0)
-                answer.Add(i);
-        }
-        return answer.ToArray();
+        int[] answer = new int[(n + 1) / 2];
+        for (int i = 0; i < (n + 1) / 2; i++)
+            answer[i] = (i << 1) + 1;
+        return answer;
     }
 }
