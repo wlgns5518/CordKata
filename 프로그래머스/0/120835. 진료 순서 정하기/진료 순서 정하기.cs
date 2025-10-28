43bddadab23290c389381562f4922e86b2dda2d6
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-28 19:04:39 +0900

    [level 0] Title: 진료 순서 정하기, Time: 4.36 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cs
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cs
@@ -1,22 +1,10 @@
 using System;
 using System.Linq;
 
-public class Solution
-{
-    public int[] solution(int[] emergency)
-    {
-        // emergency를 내림차순 정렬
-        int[] sorted = emergency.OrderByDescending(x => x).ToArray();
-
-        // 각 원소의 진료 순서를 담을 배열
-        int[] answer = new int[emergency.Length];
-
-        for (int i = 0; i < emergency.Length; i++)
-        {
-            // emergency[i]가 sorted 배열에서 몇 번째인지 찾아서 순서 지정
-            answer[i] = Array.IndexOf(sorted, emergency[i]) + 1;
-        }
-
+public class Solution {
+    public int[] solution(int[] emergency) {
+        int[] order = emergency.OrderByDescending(x => x).ToArray();
+        int[] answer = emergency.Select(x => Array.IndexOf(order, x) + 1).ToArray();
         return answer;
     }
 }
