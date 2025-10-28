b1bef3e1bb516f13880ebcc1c3ba36cb2092c39a
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-28 18:49:33 +0900

    [level 0] Title: 개미 군단, Time: 0.16 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.cs
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cs
@@ -3,28 +3,11 @@ using System;
 public class Solution {
     public int solution(int hp) {
         int answer = 0;
-        while(hp >0)
-        {
-            Console.WriteLine(hp);
-            if(hp/5 >0)
-            {
-                answer += hp/5;
-                hp %= 5;
-            }
-                
-            else if(hp/3 >0)
-            {
-                answer += hp/3;
-                hp %=3;
-            }
-                
-            else
-            {
-                answer += 1;
-                hp--;
-            }
-                
-        }
-        return answer;
+
+        int ant_5 = hp / 5;
+        int ant_3 = (hp % 5) / 3  ;
+        int ant_1 = (hp % 5) % 3 / 1 ;
+
+        return ant_5 + ant_3 + ant_1;
     }
 }
