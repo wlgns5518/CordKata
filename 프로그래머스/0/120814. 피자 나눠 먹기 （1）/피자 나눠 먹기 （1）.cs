4a52fb6663e056c7caadc449c8bede00bdda3c8b
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-28 20:14:43 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.22 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        if(n%7 >0)
+            answer += n/7 +1;
+        else
+            answer += n/7;
+        return answer;
+    }
+}
