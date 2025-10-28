02ce7bba10939ef27c91c25885fa553e8a6fc0ef
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-28 18:34:40 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.18 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cs
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cs
@@ -4,18 +4,9 @@ public class Solution
 {
     public int solution(int balls, int share)
     {
-        return (int)combination(balls, share);
-    }
-    public long combination(int num1, int num2)
-    {
         long result = 1;
-        if (num1 / 2 < num2)
-            num2 = num1 - num2;
-        for (int i = 1; i <= num2; i++)
-        {
-            result *= (num1 - i + 1);
-            result /= i;
-        }
-        return result;
+        for (int i = 1; i <= share; i++)
+            result = result * (balls - i + 1) / i;
+        return (int)result;
     }
 }
