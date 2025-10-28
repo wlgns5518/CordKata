6b99ca45e45b41e8f00ec92f5f26a32b9f25c49c
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-28 19:15:41 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.29 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.cs
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.cs
@@ -1,18 +1,14 @@
 using System;
-using System.Text;
 
 public class Solution {
     public string solution(int age) {
-        StringBuilder sb = new StringBuilder();
-        
-        while (age > 0)
+        string answer = "";
+        while(age>0)
         {
-            // age의 마지막 자리 숫자 → 문자
-            char c = (char)(age % 10 + 97);
-            sb.Insert(0, c); // 맨 앞에 삽입
-            age /= 10;
+
+            answer=(char)(age%10+97)+answer;
+            age/=10;
         }
-        
-        return sb.ToString();
+        return answer;
     }
 }
