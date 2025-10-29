984f0b81784c8b0642e2bc2b31df1d47ee785ab7
박지훈 <150890216+wlgns5518@users.noreply.github.com>
2025-10-29 14:31:45 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 7.42 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cs
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cs
@@ -5,94 +5,44 @@ using System.Linq;
 public class Solution {
     public int solution(string[] friends, string[] gifts)
 {
-    int answer = 0;
-    var friendsDicGive = new Dictionary<string, int>();
-    var friendsDicTake = new Dictionary<string, int>();
+    int n = friends.Length;
+    int[,] give = new int[n, n]; // give[i,j] = i가 j에게 준 횟수
+    int[] giveCount = new int[n]; // 준 총 횟수
+    int[] takeCount = new int[n]; // 받은 총 횟수
+    int[] nextMonth = new int[n]; // 다음달 받을 선물
     
-    for (int i = 0; i < friends.Length; i++)
+    // 인덱스 찾기용
+    var idx = new Dictionary<string, int>();
+    for (int i = 0; i < n; i++)
+        idx[friends[i]] = i;
+    
+    // 선물 기록 처리
+    foreach (var gift in gifts)
     {
-        friendsDicGive.Add(friends[i], 0);
-        friendsDicTake.Add(friends[i], 0);
+        var parts = gift.Split();
+        int from = idx[parts[0]];
+        int to = idx[parts[1]];
+        give[from, to]++;
+        giveCount[from]++;
+        takeCount[to]++;
     }
     
-    // 선물 주고받은 기록 저장
-    var giftRecord = new Dictionary<string, int>();
-    
-    for (int i = 0; i < gifts.Length; i++)
+    // 모든 쌍 비교
+    for (int i = 0; i < n; i++)
     {
-        var parts = gifts[i].Split();
-        friendsDicGive[parts[0]]++;
-        friendsDicTake[parts[1]]++;
-        
-        // "A B" 형태로 저장
-        string key = gifts[i];
-        if (!giftRecord.ContainsKey(key))
-            giftRecord[key] = 0;
-        giftRecord[key]++;
-    }
-    
-    // 선물 지수 계산
-    var giftIndex = new Dictionary<string, int>();
-    for (int i = 0; i < friends.Length; i++)
-    {
-        giftIndex[friends[i]] = friendsDicGive[friends[i]] - friendsDicTake[friends[i]];
-    }
-    
-    // 다음 달에 받을 선물 개수 계산
-    var nextMonthGifts = new Dictionary<string, int>();
-    for (int i = 0; i < friends.Length; i++)
-    {
-        nextMonthGifts[friends[i]] = 0;
-    }
-    
-    // 모든 친구 쌍 비교
-    for (int i = 0; i < friends.Length; i++)
-    {
-        for (int j = i + 1; j < friends.Length; j++)
+        for (int j = i + 1; j < n; j++)
         {
-            string personA = friends[i];
-            string personB = friends[j];
-            
-            // A가 B에게 준 횟수
-            string keyAtoB = personA + " " + personB;
-            int aToB = giftRecord.ContainsKey(keyAtoB) ? giftRecord[keyAtoB] : 0;
-            
-            // B가 A에게 준 횟수
-            string keyBtoA = personB + " " + personA;
-            int bToA = giftRecord.ContainsKey(keyBtoA) ? giftRecord[keyBtoA] : 0;
-            
-            if (aToB > bToA)
-            {
-                // A가 더 많이 줬으면 A가 받음
-                nextMonthGifts[personA]++;
-            }
-            else if (bToA > aToB)
-            {
-                // B가 더 많이 줬으면 B가 받음
-                nextMonthGifts[personB]++;
-            }
-            else
-            {
-                // 같거나 없으면 선물 지수 비교
-                if (giftIndex[personA] > giftIndex[personB])
-                {
-                    nextMonthGifts[personA]++;
-                }
-                else if (giftIndex[personB] > giftIndex[personA])
-                {
-                    nextMonthGifts[personB]++;
-                }
-            }
+            if (give[i, j] > give[j, i])
+                nextMonth[i]++;
+            else if (give[j, i] > give[i, j])
+                nextMonth[j]++;
+            else if (giveCount[i] - takeCount[i] > giveCount[j] - takeCount[j])
+                nextMonth[i]++;
+            else if (giveCount[j] - takeCount[j] > giveCount[i] - takeCount[i])
+                nextMonth[j]++;
         }
     }
     
-    // 가장 많이 받을 선물 개수
-    foreach (var count in nextMonthGifts.Values)
-    {
-        if (count > answer)
-            answer = count;
-    }
-    
-    return answer;
+    return nextMonth.Max();
 }
 }
